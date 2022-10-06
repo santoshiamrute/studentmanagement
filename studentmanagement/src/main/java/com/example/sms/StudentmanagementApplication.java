f55323ecb204ad0a1de55ee7a55750d83953c95f
santoshi <santoshi.mnnit2020@gmail.com>
2022-10-06 14:42:14 +0530

    first commit

---
+++ studentmanagement/src/main/java/com/example/sms/StudentmanagementApplication.java
@@ -0,0 +1,40 @@
+package com.example.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.sms.entity.Student;
+import com.example.sms.repository.StudentRepository;
+
+@SpringBootApplication
+public class StudentmanagementApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmanagementApplication.class, args);
+	}
+
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		/*
+		 * Student student1 = new Student("Ramesh", "Fadatare", "ramesh@gmail.com");
+		 * studentRepository.save(student1);
+		 * 
+		 * Student student2 = new Student("Sanjay", "Jadhav", "sanjay@gmail.com");
+		 * studentRepository.save(student2);
+		 * 
+		 * Student student3 = new Student("tony", "stark", "tony@gmail.com");
+		 * studentRepository.save(student3);
+		 */
+		
+	}
+
+}
---
+++ studentmanagement/src/main/java/com/example/sms/entity/Student.java
@@ -0,0 +1,78 @@
+package com.example.sms.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "phoneno")
+	private String phoneno;
+	
+	
+	public Student() {
+		
+	}
+	
+	public Student(String firstName,String email, String password, String phoneno ) {
+		super();
+		this.firstName = firstName;
+		this.email = email;
+		this.password = password;
+		this.phoneno = phoneno;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	 
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhoneno() {
+		return phoneno;
+	}
+
+	public void setPhoneno(String phoneno) {
+		this.phoneno = phoneno;
+	}
+	
+}
